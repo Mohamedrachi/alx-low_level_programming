9cf0b1642464a8ce5b8bdb7e659242b23ecf17b3
Mohamedrachi <jaamkaal80@gmail.com>
2022-06-08 15:33:05 -0700

    bismillah

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,20 @@
 #include "main.h"
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints putchar
+ * reaturn value 0
+ * return: alwys 0
  */
 int main(void)
 {
-	char *sh = "Holberton";
-
-        while (*sh)
-        {
-               
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
