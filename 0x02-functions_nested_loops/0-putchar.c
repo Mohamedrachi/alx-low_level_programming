84146173efde183b9f5d3a084657efded5ff3458
Mohamedrachi <jaamkaal80@gmail.com>
2022-06-08 14:55:27 -0700

    good

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * main - prints putchar
- * reaturn value 0
- * return: alwys 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char *sh = "Holberton";
+
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
+
+	return (0);
 }
