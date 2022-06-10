e3a5e2983789903b9317054016e357510d08dab6
Mohamedrachi <jaamkaal80@gmail.com>
2022-06-09 22:28:16 -0700

    yes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
 /**
- * print_numbers -  checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
+ * print_numbers -> print numbers
  */
+
 void print_numbers(void)
 {
-	int a;
+	int x;
 
-	for (a = 0; a < 10; a++)
-	{
-		_putchar(a + '0');
-	}
+	for (x = 0; x < 10; x++)
+		_putchar(x + '0');
 	_putchar('\n');
-}	
+}
